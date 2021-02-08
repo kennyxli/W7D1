f5c60d9050a71a4a182e9492f337e53e3f4d58b1
Kenny Li <kenny.wj.li@gmail.com>
2021-02-08 16:23:09 -0500

    Finished application_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,16 +6,13 @@ class SessionsController < ApplicationController
     end
     def create
         @user = User.find_by_credentials(params[:user][:user_name], params[:user][:password])
-
         if @user
-            login!(@user)
+            login_user!(@user)
             redirect_to cats_url
-            #Do we want to redirect to cats index page or user showpage
         else
             @user = User.new
             render :new
         end
-        #redirect_to cats_url
     end
     def destroy
         logout!
