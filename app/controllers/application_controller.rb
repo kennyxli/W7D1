be19722e160176a247fa7c96433b4bcfbe4cf72a
Kenny Li <kenny.wj.li@gmail.com>
2021-02-08 15:08:38 -0500

    finished session new view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+    def login!(user)
+        session[:session_token] = user.reset_session_token!
+        #clarify why we're updating the session hash
+    end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,19 @@ class SessionsController < ApplicationController
     end
     def create
         @user = User.find_by_credentials(params[:user][:user_name], params[:user][:password])
-    end
 
+        if @user
+            login!(@user)
+            redirect_to cats_url
+            #Do we want to redirect to cats index page or user showpage
+        else
+            @user = User.new
+            render :new
+        end
+        #redirect_to cats_url
+    end
+    def destroy
+        logout!
+        redirect_to new_session_url
+    end
 end
